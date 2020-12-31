b1133c3db280b048339e8c87afba3e6d486c092b
zhanglishuai <1824169292@qq.com>
2020-12-31 17:30:33 +0800

    1111

--- src/main/java/com/yinggu/Happy.java
+++ src/main/java/com/yinggu/Happy.java
@@ -3,6 +3,6 @@ package com.yinggu;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("happy new year");
-		System.err.println("I need help");
+		System.err.println("I need help yet");
 	}
 }
